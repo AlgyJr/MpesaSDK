1fb0fff6723635450624fa52913882e532c58ca0
ALgy Aly <algymussa@gmail.com>
2022-01-17 02:19:30 +0200

    doc: add documentation

--- Sources/MpesaSDK/Models/PaymentRequest.swift
+++ Sources/MpesaSDK/Models/PaymentRequest.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+/// Struct with all the fields required to perfom a Customer-to-Business (C2B) or Business-to-Customer (B2C) transaction Payment to M-Pesa API
+/// @transactionReference: This is the reference of the transaction for the customer or business making the transaction. This can be a smartcard number for a TV subscription or a reference number of a utility bill
+/// @customerMSISDN: MSISDN of the customer for the transaction
+/// @amount: The amount for the transaction
+/// @thirdPartyReference: This is the unique reference of the third party system. When there are queries about transactions, this will usually be used to track a transaction
+/// @serviceProviderCode: Shortcode of the business where funds will be credited to
+
 public struct PaymentRequest: Encodable {
     public let transactionReference: String
     public let customerMSISDN      : String
