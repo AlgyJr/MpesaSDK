2d0376b5c3ae67f4ddb9614504ccc1784d674a88
ALgy Aly <algymussa@gmail.com>
2022-01-19 13:23:27 +0200

    chore(package): add platforms parameter
    Set platforms parameter

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "MpesaSDK",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_12),
+        .watchOS(.v3),
+        .tvOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
