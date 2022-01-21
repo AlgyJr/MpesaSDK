579203b687823db41a6ddb9f4580cee97b4c93ce
ALgy Aly <algymussa@gmail.com>
2022-01-21 02:02:33 +0200

    feat: add some errors enum

---
+++ Sources/MpesaSDK/Utils/MpesaError.swift
@@ -0,0 +1,17 @@
+//
+//  MpesaError.swift
+//  
+//
+//  Created by ALgy Aly on 17/01/22.
+//
+
+import Foundation
+
+enum MpesaError: String, Error {
+    case invalidApiAddress = "Invalid API Address"
+    case invalidURL        = "Invalid URL"
+    case invalidToken      = "API Key or Public Key incorrect"
+    case missingData       = "The data couldn't be read because it is missing"
+    case unableToEncode    = "Unable to encode payment request to JSON"
+    case outputError       = "An error occurred on request"
+}
