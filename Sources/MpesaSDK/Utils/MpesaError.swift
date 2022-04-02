965680133ce569569cb80f827d8b97091fd9514d
ALgy Aly <algymussa@gmail.com>
2022-04-02 05:14:31 +0200

    refactor: add unknow error and edit some error message

--- Sources/MpesaSDK/Utils/MpesaError.swift
+++ Sources/MpesaSDK/Utils/MpesaError.swift
@@ -32,8 +32,9 @@ import Foundation
 public enum MpesaError: String, Error {
     case invalidApiAddress = "Invalid API Address"
     case invalidURL        = "Invalid URL"
-    case invalidToken      = "API Key or Public Key incorrect"
+    case invalidToken      = "API Key or Public Key invalid"
     case missingData       = "The data couldn't be read because it is missing"
     case unableToEncode    = "Unable to encode payment request to JSON"
     case outputError       = "An error occurred on request"
+    case unknownError      = ""
 }
