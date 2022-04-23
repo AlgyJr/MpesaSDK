f531dd1d6ec06762ad63df51b59ed8b9c96cb727
ALgy Aly <algymussa@gmail.com>
2022-04-23 04:38:32 +0200

    feat: add phone number formatter

---
+++ Sources/MpesaSDK/Utils/PhoneNumberHelper.swift
@@ -0,0 +1,49 @@
+//
+//  PhoneNumberHelper.swift
+//  
+//
+//  Created by ALgy Aly on 23/04/22.
+//
+//  MIT License
+//
+//  Copyright (c) 2022 Algy Ali
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import Foundation
+
+class PhoneNumberHelper {
+    // The MSISDN sending the funds
+    static func formatPhoneNumber(_ phoneNumber: String) -> String {
+        if phoneNumber.isEmpty {
+            return ""
+        }
+        
+        if phoneNumber.count == 9 && phoneNumber.starts(with: "8") {
+            return "258\(phoneNumber)"
+        }
+        
+        if phoneNumber.count == 13 && phoneNumber.starts(with: "+") {
+            return String(phoneNumber.dropFirst())
+        }
+        
+        return phoneNumber
+    }
+}
---
+++ Tests/MpesaSDKTests/Tests/PhoneNumberHelperTests.swift
@@ -0,0 +1,72 @@
+//
+//  PhoneNumberHelperTests.swift
+//  
+//
+//  Created by ALgy Aly on 23/04/22.
+//
+//  MIT License
+//
+//  Copyright (c) 2022 Algy Ali
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
+import XCTest
+@testable import MpesaSDK
+
+class PhoneNumberHelperTests: XCTestCase {
+    func testFormatingPhoneNumberWithPlusCountryCodePrefix() {
+        // Given
+        let phoneNumber: String = "+258849022333"
+        
+        // When
+        let result = PhoneNumberHelper.formatPhoneNumber(phoneNumber)
+        
+        // Then
+        let expectedResult = "258849022333"
+        
+        XCTAssert(result == expectedResult)
+    }
+    
+    func testFormatingPhoneNumberWithCountryCodePrefix() {
+        // Given
+        let phoneNumber: String = "258849022333"
+        
+        // When
+        let result = PhoneNumberHelper.formatPhoneNumber(phoneNumber)
+        
+        // Then
+        let expectedResult = "258849022333"
+        
+        XCTAssert(result == expectedResult)
+    }
+    
+    func testFormatingPhoneNumberWithoutCountryCodePrefix() {
+        // Given
+        let phoneNumber: String = "849022333"
+        
+        // When
+        let result = PhoneNumberHelper.formatPhoneNumber(phoneNumber)
+        
+        // Then
+        let expectedResult = "258849022333"
+        
+        XCTAssert(result == expectedResult)
+    }
+}
